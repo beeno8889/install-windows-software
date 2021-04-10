ba3e4d66e3c63c5415887b1067e90ac5764ec7a6
beeno <bitmaro@protonmail.com>
2021-04-10 16:22:27 +1200

    Add install script

---
+++ scripts/InstallSoftware.ps1
@@ -0,0 +1,30 @@
+# Need to run Set-ExecutionPolicy Unrestricted
+param ($sourceURL='https://commondatastorage.googleapis.com/chromium-browser-snapshots/Win_x64/871178/chrome-win.zip', $destinationFolder='C:\Program Files')
+
+$Logfile="C:\temp\install-$(get-date -f MM-dd-yyyy_HH_mm_ss).log"
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+
+try {
+abc
+
+$tempFilePath='C:\temp\temp.zip'
+$downloadFolder='C:\temp\downloadfolder'
+
+$webClient = New-Object -TypeName System.Net.WebClient
+$webClient.DownloadFile($sourceURL, $tempFilePath)
+Expand-Archive -Path $tempFilePath -DestinationPath $downloadFolder
+ls $downloadFolder
+mv $downloadFolder\* $destinationFolder
+
+# Clean up
+rm $tempFilePath
+rmdir $downloadFolder
+} catch {
+Write-Host "An error occurred, check the log file $Logfile"
+LogWrite "ERROR: $_"
+}
